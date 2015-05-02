bb54dab0e4e47d88f6edbbad284f91763aa2d387
isishizuka <isishizuka@gmail.com>
2015-05-02 18:33:35 +0900

    コメント読み取り機能の実装

--- LanguageTranslator/CodeCommentParser/Parser.cs
+++ LanguageTranslator/CodeCommentParser/Parser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LanguageTranslator.CodeCommentParser
 {
@@ -10,7 +11,15 @@ namespace LanguageTranslator.CodeCommentParser
 	{
 		public void Parse(string xmlPath, Definition.Definitions definisions)
 		{
+			var document = XDocument.Load(xmlPath);
+			
+			var summaries = document.Descendants("member")
+				.Select(m => new { Address = m.Attribute("name").Value.ToString(), Summary = m.Elements("summary") })
+				.Where(fs => fs.Summary.Any())
+				.ToDictionary(fs => fs.Address, fs => fs.Summary.Single().Value.ToString());
 
+			definisions.Classes.ForEach(c => { if (summaries.ContainsKey(c.Name)) c.Brief = summaries[c.Name]; });
+			definisions.Enums.ForEach(e => { if (summaries.ContainsKey(e.Name)) e.Brief = summaries[e.Name]; });
 		}
 	}
 }
