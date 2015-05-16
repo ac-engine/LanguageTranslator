5fe93f7fb11bf5f8ec2cd4df0393cc1fd22f16dc
ArGxento <ArGxento@gmail.com>
2015-05-16 23:12:32 +0900

    Translator.Javaのenumをほぼ実装、classを実装中

--- LanguageTranslator/Translator/Java/Translator.cs
+++ LanguageTranslator/Translator/Java/Translator.cs
@@ -8,9 +8,66 @@ namespace LanguageTranslator.Translator.Java
 {
 	class Translator : ITranslator
 	{
+		private StringBuilder Res = new StringBuilder();
+		private int IndentDepth = 0;
+		private void MakeIndent()
+		{
+			Res.Append('\t', IndentDepth);
+		}
+
+		private void MakeBrief(string brief)
+		{
+			if (String.IsNullOrEmpty(brief))
+			{
+				return;
+			}
+			MakeIndent();
+			Res.AppendFormat("/* {0} */\n", brief);
+		}
+
+		private void OutputEnum(Definition.EnumDef es)
+		{
+			MakeBrief(es.Brief);
+			MakeIndent();
+			Res.AppendFormat("enum {0} {{\n", es.Name);
+			IndentDepth++;
+			foreach (var e in es.Members) 
+			{
+				MakeBrief(e.Brief);
+				MakeIndent();
+				Res.AppendFormat("{0} = {1},\n", e.Name, e.Value.ToString());
+			}
+			IndentDepth--;
+			MakeIndent();
+			Res.AppendFormat("}}");
+		}
+
+
+		private void OutputClass(Definition.ClassDef cs)
+		{
+			MakeBrief(cs.Brief);
+			MakeIndent();
+			Res.AppendFormat("class {0} {{\n", cs.Name);
+			IndentDepth++;
+			foreach (var p in cs.Properties)
+			{
+				MakeBrief(p.Brief);
+				MakeIndent();
+				Res.AppendFormat("private {0} {1};", p.);
+			}
+			IndentDepth--;
+			MakeIndent();
+			Res.AppendFormat("}}");
+		}
 		public void Translate(string targetDir, Definition.Definitions definisions)
 		{
+			foreach (Definition.EnumDef e in definisions.Enums)
+			{
+				OutputEnum(e);
+			}
 
+			Console.Write(Res.ToString());
+			Console.ReadLine();
 		}
 	}
 }
