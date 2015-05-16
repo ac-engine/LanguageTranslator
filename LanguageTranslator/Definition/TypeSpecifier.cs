18f0ebb4b050f4c2df45fa89819139f2c6a029f8
NumAniCloud <num.ani.cloud@gmail.com>
2015-05-16 22:55:02 +0900

    TypeSpecifierクラス群を追加

---
+++ LanguageTranslator/Definition/TypeSpecifier.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageTranslator.Definition
+{
+	abstract class TypeSpecifier
+	{
+	}
+
+	class SimpleType : TypeSpecifier
+	{
+		public string Type = string.Empty;
+
+		public override string ToString()
+		{
+			return "SimpleType " + Type;
+		}
+	}
+
+	class ArrayType : TypeSpecifier
+	{
+		public string BaseType = string.Empty;
+
+		public override string ToString()
+		{
+			return string.Format("ArrayType {0}[]", BaseType);
+		}
+	}
+
+	class GenericType : TypeSpecifier
+	{
+		public string OuterType = string.Empty;
+		public List<string> InnerType = new List<string>();
+
+		public override string ToString()
+		{
+			return string.Format("GenericType {0}<{1}>", OuterType, string.Join(",", InnerType));
+		}
+	}
+
+	class NullableType : TypeSpecifier
+	{
+		public string BaseType = string.Empty;
+
+		public override string ToString()
+		{
+			return string.Format("NullableType {0}?", BaseType);
+		}
+	}
+}
